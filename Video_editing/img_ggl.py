0d4f6cffc4f7a50d6e176e1e875306e9b4ea563f
RAVI KUMAR GAUTAM <69303731+RGIIST@users.noreply.github.com>
2023-08-16 01:45:11 +0530

    Update img_ggl.py
    Download images from google search

--- Video_editing/img_ggl.py
+++ Video_editing/img_ggl.py
@@ -1,5 +1,5 @@
 from simple_image_download import simple_image_download
 
-keyword,limit = "overcast weather car", 500
+keyword,limit = "", 500
 response=simple_image_download.simple_image_download
 response().download(keyword,limit)
